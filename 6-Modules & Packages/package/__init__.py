3b22ef8f481822192e46071c85ebef5d0813ff11
AsifAslamOvi <asifaslamovi@gmail.com>
2026-04-17 22:49:57 +0600

    pushing 6th and 7th folder

---
+++ 6-Modules & Packages/package/__init__.py
@@ -0,0 +1,9 @@
+# Package initialization file
+'''
+__init__.py is a special file in Python that is used to mark a directory as a package. 
+It can be an empty file, but it can also contain initialization code for the package. 
+When you import a package, the code in __init__.py is executed, 
+allowing you to set up any necessary variables, functions, or classes that should be available when 
+the package is imported.
+
+'''
---
+++ 6-Modules & Packages/test.py
@@ -0,0 +1,10 @@
+from package.maths import *
+from package.subpackages.find_max import max_number
+
+
+print(addition(4, 9))
+print(subtraction(4, 9))
+print(multiplication(4, 9))
+print(division(4, 9))
+
+print(max_number(4, 9))
